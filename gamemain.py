9fba1269ce3e858735f7ede6dde62a7f77ea4f7c
Christopher Dunkling <moaw123@gmail.com>
2016-11-01 19:36:34 +0000

    window with white background limited to 60fps

--- gamemain.py
+++ gamemain.py
@@ -10,5 +10,19 @@ RED      = ( 255,   0,   0)
 BLUE     = (   0,   0, 255)
 
 size = (700, 500)
-while True:
-    screen =pygame.display.set_mode(size)
+done = False
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode(size)##, pygame.FULLSCREEN)
+pygame.display.set_caption("Killer Robot Simulator 2K16")
+
+
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    screen.fill(WHITE)
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
