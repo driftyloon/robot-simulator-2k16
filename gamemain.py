4a9c4a5c2b32fa82ed90b1d2f5572a8d480c1346
Christopher Dunkling <moaw123@gmail.com>
2016-11-01 18:22:17 +0000

    updated with python file which creates a window

---
+++ gamemain.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+
+size = (700, 500)
+while True:
+    screen =pygame.display.set_mode(size)
