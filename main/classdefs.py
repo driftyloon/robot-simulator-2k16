2cff1490bedc3920766391b026ab32e649f729ee
Christopher Dunkling <dunklingc@gmail.com>
2016-11-06 03:47:49 +0000

    new weapons and sounds as well as a game over state

--- main/classdefs.py
+++ main/classdefs.py
@@ -12,6 +12,7 @@ class killerrobot(pygame.sprite.Sprite):
         self.rect.top = 200
         self.speed=4
         self.angle = 0
+        self.health=100
 
     def rotate(self,mousepos):
         self.angle = math.degrees(math.atan2(self.rect.centerx - mousepos[0], self.rect.centery - mousepos[1]))
@@ -19,9 +20,17 @@ class killerrobot(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(self.orgimage, self.angle)
 
         self.rect = self.image.get_rect(center=self.rect.center)
-
+    def takedamage(self,damage):
+        self.health-=damage
+        self.rect.centerx+=50
+        self.rect.centery+=50
+        if self.health>0:
+            return True
+        else:
+            return False
     def getangle(self):
         return self.angle
+
     def getpos(self):
         return [self.rect.centerx,self.rect.centery]
 
@@ -54,6 +63,36 @@ class killerrobot(pygame.sprite.Sprite):
         else:
             self.rect.bottom += self.speed
 
+class Text(pygame.sprite.Sprite):
+    def __init__(self, size, color, position,variable,message, alpha):
+        '''accepts size,color,position,variables,message and alpha'''
+        pygame.sprite.Sprite.__init__(self)
+        self.__font = pygame.font.Font('./main/AmazDooMRight2.ttf', size)
+        self.__color = color
+        self.__position = position
+        self.variable=variable
+
+        self.message = message
+        self.m = ''
+        self.__alpha = alpha
+
+    def set_variable(self,variable):
+        if variable:
+            self.variable=variable
+    def setalpha(self,alpha):
+            self.__alpha=alpha
+
+    def update(self):
+        '''This method will be called automatically to display
+        the text at the set position.'''
+        if self.variable!=None:
+            self.m = self.message+self.variable
+        else:
+            self.m=self.message
+        self.image = self.__font.render(self.m, 1, self.__color)
+        self.image.set_alpha(self.__alpha)
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.__position)
 
 class Bullet(pygame.sprite.Sprite):
 
@@ -61,6 +100,7 @@ class Bullet(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.x = player_pos[0]
         self.y = player_pos[1]
+        self.damage=damage
 
         # Assign the mouse target position
         self.targetx = mouse_pos[0]
@@ -85,6 +125,9 @@ class Bullet(pygame.sprite.Sprite):
         self.animation_steps = self.__distance / speed
         self.dx = (self.targetx - self.x) / self.animation_steps
         self.dy = (self.targety - self.y) / self.animation_steps
+
+    def getdamage(self):
+        return self.damage
 
     def update(self):
         self.rect.centerx += self.dx
@@ -144,3 +187,137 @@ class enemy(pygame.sprite.Sprite):
     def update(self):
             self.rect.centerx += self.__dx
             self.rect.centery += self.__dy
+class smartenemy(enemy):
+    def __init__(self,image,screen):
+        pygame.sprite.Sprite.__init__(self)
+        self.screen=screen
+        if image:
+            self.image=image
+            self.org=image
+            self.rect = self.image.get_rect()
+            self.rect.center=(200,200)
+            self.rect = self.image.get_rect(center=self.rect.center)
+            self.spawn()
+            self.look()
+            self.set_step_amount((215,152))
+        else:
+            self.org=pygame.Surface((200,100))
+            self.org.fill((255,0,0))
+            self.org.set_alpha(0)
+            self.rect = self.org.get_rect()
+            self.rect.center=(200,200)
+            self.spawn()
+            self.look()
+            self.set_step_amount((215,152))
+    def look(self):
+        self.rotate((120,255))
+        self.rotate((120, 555))
+
+    def set_step_amount(self, player_pos):
+        try:
+            self.distance = math.sqrt \
+                (pow(player_pos[0] - self.rect.centerx, 2) + pow(player_pos[1] - self.rect.centery, 2))
+            self.__animation_steps = self.distance / self.speed
+            self.__dx = (player_pos[0] - self.rect.centerx) / self.__animation_steps
+            self.__dy = (player_pos[1] - self.rect.centery) / self.__animation_steps
+        except:
+            self.__dx = 0
+            self.__dy = 0
+
+    def rotate(self,playerpos):
+        self.__angle = math.degrees(math.atan2 \
+                                        (self.rect.centerx - playerpos[0], self.rect.centery - playerpos[1]))
+
+        self.image = pygame.transform.rotate \
+            (self.org, self.__angle)
+
+        self.rect = self.image.get_rect(center=self.rect.center)
+    def getangle(self):
+        return self.__angle
+
+    def damage(self,damagedone):
+        self.health-=damagedone
+        if self.health>0:
+            return True
+        else:
+            return False
+
+    def update(self):
+            self.look()
+            self.rect.centerx += self.__dx
+            self.rect.centery += self.__dy
+
+
+class shotgunpickup(pygame.sprite.Sprite):
+    def __init__(self, image,screen):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.image.convert_alpha()
+        self.org = self.image
+        self.rect = self.image.get_rect()
+        self.screen = screen
+        self.spawn()
+
+    def spawn(self):
+        self.rect.center = (300,200)
+
+
+class Explosions(pygame.sprite.Sprite):
+    def __init__(self, image,screen,position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.image.convert_alpha()
+        self.org = self.image
+        self.rect = self.image.get_rect()
+        self.screen = screen
+        self.spawn(position)
+
+    def spawn(self,pos):
+        self.rect.center = pos
+        newtime = pygame.time.get_ticks()
+        oldtime = newtime
+        while newtime-oldtime<1000:
+            newtime = pygame.time.get_ticks()
+            if newtime-oldtime>=100:
+               self.kill()
+               break
+
+class Rocket(Bullet,pygame.sprite.Sprite):
+    def donothing(self):
+        x=0
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, [255,255,255])
+    return textSurface, textSurface.get_rect()
+
+def message_display(text,screen):
+    largeText = pygame.font.Font('AmazDooMRight2.ttf',115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((1024/2),(768/2))
+    screen.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    pygame.time.sleep(2)
+
+class button(pygame.sprite.Sprite):
+    def __init__(self,msg,x,y,w,h,ic,ac,screen):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((5, 5))
+        self.image.fill((255, 0, 0))
+        self.image.set_alpha(0)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        print(click)
+        if x+w > mouse[0] > x and y+h > mouse[1] > y:
+            pygame.draw.rect(screen,ac,(x,y,w,h))
+        else:
+            pygame.draw.rect(screen, ic,(x,y,w,h))
+
+        smallText = pygame.font.SysFont("AmazDooMRight2.ttf",20)
+        textSurf,textRect = text_objects(msg, smallText)
+        textRect.center = ( (x+(w/2)), (y+(h/2)) )
+        screen.blit(textSurf, textRect)
--- main/robotseekndestroy.py
+++ main/robotseekndestroy.py
@@ -2,24 +2,138 @@ import pygame,pygame.locals,random,os,math
 import sys
 
 from main import classdefs
+
 pygame.mixer.init()
 pygame.mixer.music.load("./main/Contra Hard Corps Music.mp3")
-pygame.mixer.music.play(-1)
+pygame.font.init()
+##pygame.mixer.music.play(-1)
+fire = pygame.mixer.Sound("./main/DSPISTOL.wav")
+fireshotgun = pygame.mixer.Sound("./main/DSSHOTGN.wav")
+firerocket = pygame.mixer.Sound("./main/DSRLAUNC.wav")
+fireplasma = pygame.mixer.Sound("./main/DSPLASMA.wav")
+explode= pygame.mixer.Sound("./main/DSBAREXP.wav")
+pain= pygame.mixer.Sound("./main/DSPLPAIN.wav")
+playerdeath=pygame.mixer.Sound("./main/DSPLDETH.wav")
 pygame.display.set_caption("Robot simulator 2016")
 clock = pygame.time.Clock()
 screen=pygame.display.set_mode((1024,768))
 rect=screen.get_rect()
-running=True
-bullet_hitbox=pygame.sprite.Group()
-bullet_img=pygame.sprite.Group()
-enemies=pygame.sprite.Group()
-x=1
-robot= classdefs.killerrobot('./main/temp_sprite.png')
-robot_group = pygame.sprite.RenderPlain(robot)
-allSprites = pygame.sprite.OrderedUpdates\
-    (bullet_img,bullet_hitbox,robot,enemies)
-while running:
-    clock.tick(30)
+black = (0,0,0)
+white = (255,255,255)
+green = (0,200,0)
+red = (200,0,0)
+bullet_hitbox = pygame.sprite.Group()
+bullet_img = pygame.sprite.Group()
+rocket_hitbox = pygame.sprite.Group()
+rocket_img = pygame.sprite.Group()
+powerups = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
+robot_group = pygame.sprite.Group()
+smartenemies = pygame.sprite.Group()
+explosions = pygame.sprite.Group()
+enemy_hitbox = pygame.sprite.Group()
+warnings = pygame.sprite.Group()
+global x
+global score
+def reset():
+    global rapidfire
+    global rapidfire2
+    global lastfired
+    global gotshotgun
+    global gotmachinegun
+    global gotplasmarifle
+    global gotrocketlauncher
+    global running
+    global machinegundelay
+    global plasmarifledelay
+    global weaponselected
+    global allSprites
+    global x
+    global score
+    global robot
+    rapidfire=False
+    rapidfire2=False
+    lastfired=0;
+    gotshotgun=False
+    gotmachinegun=True
+    gotplasmarifle=True
+    gotrocketlauncher=True
+    running=True
+    machinegundelay=0
+    plasmarifledelay=0
+    weaponselected=1
+    bullet_hitbox.empty()
+    bullet_img.empty()
+    rocket_hitbox.empty()
+    rocket_img.empty()
+    powerups.empty()
+    enemies.empty()
+    robot_group.empty()
+    explosions.empty()
+    enemy_hitbox.empty()
+    warnings.empty()
+    x=1
+    score=0
+    allSprites = pygame.sprite.OrderedUpdates \
+        (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies,powerups,explosions)
+    robot = classdefs.killerrobot('./main/temp_sprite.png')
+    robot_group.add(robot)
+    shotgun = classdefs.shotgunpickup(pygame.image.load('./main/shotgunicon.png'), screen)
+    powerups.add(shotgun)
+    enemy1 = classdefs.enemy(pygame.image.load('./main/temp_sprite.png'), robot.rect.center, screen)
+    enemies.add(enemy1)
+    print("lo")
+
+
+
+
+def GameOver(score, screen):
+    finalscore = classdefs.Text(50, (0, 0, 0), (500, 400), str(score), 'F i n a l   S c o r e :  ', 255)
+    screen.fill([255, 255, 255])
+    fs = pygame.sprite.Group()
+    buttons = pygame.sprite.Group()
+    fs.add(finalscore)
+    button1=classdefs.button("Play Again", 150, 450, 100, 50, green, white,screen)
+    button2 = classdefs.button("Quit", 550, 450, 100, 50, red, black,screen)
+    buttons.add(button1)
+    buttons.add(button2)
+    display = pygame.sprite.OrderedUpdates \
+        (fs,buttons)
+    display.update()
+    display.draw(screen)
+    pygame.display.flip()
+    x=True
+
+    while x==True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    sys.exit(0)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print(pygame.mouse.get_pos())
+                if pygame.mouse.get_pos()[0]>= 150 and pygame.mouse.get_pos()[0]<= 250:
+                    if pygame.mouse.get_pos()[1]>= 450 and pygame.mouse.get_pos()[1]<= 500:
+                        x=False
+                        running==True
+                        reset()
+                elif pygame.mouse.get_pos()[0]>= 550 and pygame.mouse.get_pos()[0]<= 650:
+                    if pygame.mouse.get_pos()[1]>= 450 and pygame.mouse.get_pos()[1]<= 500:
+                        sys.exit(0)
+            elif event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        clock.tick(60)
+        display.update()
+        display.draw(screen)
+        pygame.display.flip()
+wave_text = classdefs.Text(50, (0, 0, 0), (500, 50), 0, 'Score:', 255)
+reset()
+allSprites = pygame.sprite.OrderedUpdates \
+    (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies, wave_text, powerups, explosions)
+wave_text.set_variable(str(score))
+allSprites.draw(screen)
+while running==True:
+    clock.tick(60)
     keystate = pygame.key.get_pressed()
     if keystate[pygame.locals.K_w]:
         robot.go_up(screen)
@@ -29,28 +143,229 @@ while running:
         robot.go_down(screen)
     if keystate[pygame.locals.K_d]:
         robot.go_right(screen)
+    newtime = pygame.time.get_ticks()
+
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            sys.exit(0)
+        oldtime = newtime
+        newtime = pygame.time.get_ticks()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                sys.exit(0)
+            elif event.key == pygame.K_1:
+                rapidfire = False
+                rapidfire2= False
+                weaponselected = 1
+            elif event.key == pygame.K_2:
+                rapidfire = False
+                rapidfire2= False
+                if gotshotgun==True:
+                    weaponselected = 2
+                else:
+                    noshotgun = classdefs.Text(50, (0, 0, 0), (500, 205), None, 'You dont have the shotgun!', 255)
+                    warnings.add(noshotgun)
+                    allSprites = pygame.sprite.OrderedUpdates \
+                        (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies, wave_text,
+                         powerups,explosions)
+                    starttime = pygame.time.get_ticks()
+                    newtime = 0
+                    while starttime - newtime <= 9000:
+                        newtime = pygame.time.get_ticks()
+                    allSprites = pygame.sprite.OrderedUpdates \
+                        (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies, wave_text,
+                         powerups,explosions)
+                    weaponselected = 1
+
+            elif event.key == pygame.K_3:
+                rapidfire = False
+                rapidfire2= False
+                if gotmachinegun == True:
+                    weaponselected = 3
+                    rapidfire==False
+                else:
+                    nomg = classdefs.Text(50, (0, 0, 0), (500, 502), None, 'You dont have the rocketlauncher!', 255)
+                    warnings.add(norl)
+                    allSprites = pygame.sprite.OrderedUpdates \
+                        (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies, wave_text,
+                         powerups,explosions)
+                    starttime = pygame.time.get_ticks()
+                    newtime = 0
+                    while starttime - newtime < 900:
+                        newtime = pygame.time.get_ticks()
+                        if starttime-newtime<900:
+                            nomg.kill()
+                    weaponselected = 1
+
+            elif event.key == pygame.K_4:
+                rapidfire = False
+                rapidfire2= False
+                if gotrocketlauncher == True:
+                    weaponselected = 4
+                else:
+                    norl = classdefs.Text(50, (0, 0, 0), (500, 502), None, 'You dont have the rocketlauncher!', 255)
+                    warnings.add(norl)
+                    allSprites = pygame.sprite.OrderedUpdates \
+                        (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies, wave_text,
+                         powerups,explosions)
+                    starttime = pygame.time.get_ticks()
+                    newtime = 0
+                    while starttime - newtime < 900:
+                        newtime = pygame.time.get_ticks()
+                        if starttime-newtime<900:
+                            norl.kill()
+                    weaponselected = 1
+            elif event.key == pygame.K_5:
+                if gotplasmarifle == True:
+                    weaponselected = 5
+                    rapidfire = False
+                    rapidfire2 = False
+                else:
+                    nopr = classdefs.Text(50, (0, 0, 0), (500, 502), None, 'You dont have the rocketlauncher!', 255)
+                    warnings.add(nopr)
+                    allSprites = pygame.sprite.OrderedUpdates \
+                        (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies, wave_text,
+                         powerups,explosions)
+                    starttime = pygame.time.get_ticks()
+                    newtime = 0
+                    while starttime - newtime < 900:
+                        newtime = pygame.time.get_ticks()
+                        if starttime-newtime<900:
+                            nopr.kill()
+                    weaponselected = 1
+
+
+
         elif  event.type == pygame.MOUSEBUTTONDOWN:
-            bullet1 = classdefs.Bullet \
-                (pygame.image.load('./main/shot.png'), robot.getangle(), \
-                 robot.rect.center, pygame.mouse.get_pos(), 10, 5)
-            bullet2 = classdefs.Bullet \
-                (None, None, robot.rect.center, pygame.mouse.get_pos(), 10, 5)
+            if weaponselected==1 and pygame.time.get_ticks() - lastfired >= 500:
+                lastfired = pygame.time.get_ticks()
+                fire.play(0)
+                bullet1 = classdefs.Bullet \
+                    (pygame.image.load('./main/shot.png'), robot.getangle(), \
+                     robot.rect.center, pygame.mouse.get_pos(), 10, 5)
+                tracer1 = classdefs.Bullet \
+                    (None, None, robot.rect.center, pygame.mouse.get_pos(), 10, 5)
+                bullet_img.add(bullet1)
+                bullet_hitbox.add(tracer1)
+                allSprites = pygame.sprite.OrderedUpdates \
+                    (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies, wave_text, powerups,
+                    explosions)
+            elif gotshotgun==True and weaponselected==2 and pygame.time.get_ticks() - lastfired >= 1200:
+                lastfired = pygame.time.get_ticks()
+                fireshotgun.play(0)
+                bullet1 = classdefs.Bullet \
+                    (pygame.image.load('./main/shot.png'), robot.getangle(), \
+                     robot.rect.center, (pygame.mouse.get_pos()[0]+200,pygame.mouse.get_pos()[1]+200), 10, 7)
+                tracer1 = classdefs.Bullet \
+                    (None, None, robot.rect.center, (pygame.mouse.get_pos()[0]+200,pygame.mouse.get_pos()[1]+200), 10, 7)
+                bullet_img.add(bullet1)
+                bullet_hitbox.add(tracer1)
+                bullet2 = classdefs.Bullet \
+                    (pygame.image.load('./main/shot.png'), robot.getangle(), \
+                     robot.rect.center, pygame.mouse.get_pos(), 10, 7)
+                tracer2 = classdefs.Bullet \
+                    (None, None, robot.rect.center, pygame.mouse.get_pos(), 10, 7)
+                bullet_img.add(bullet2)
+                bullet_hitbox.add(tracer2)
+                bullet3 = classdefs.Bullet \
+                    (pygame.image.load('./main/shot.png'), robot.getangle(), \
+                     robot.rect.center, (pygame.mouse.get_pos()[0]-200,pygame.mouse.get_pos()[1]+200), 10, 7)
+                tracer3 = classdefs.Bullet \
+                    (None, None, robot.rect.center,( pygame.mouse.get_pos()[0]-200,pygame.mouse.get_pos()[1]+200), 10, 7)
+                bullet_img.add(bullet3)
+                bullet_hitbox.add(tracer3)
+                allSprites = pygame.sprite.OrderedUpdates \
+                    (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies, wave_text, powerups,
+                    explosions)
+            elif gotmachinegun == True and weaponselected == 3:
+                rapidfire= True
+            elif gotrocketlauncher==True and weaponselected==4 and pygame.time.get_ticks() - lastfired >= 1000:
+                lastfired = pygame.time.get_ticks()
+                firerocket.play(0)
+                rocket = classdefs.Rocket \
+                    (pygame.image.load('./main/rocket.png'), robot.getangle(), \
+                     robot.rect.center, pygame.mouse.get_pos(), 10, 5)
+                tracer1 = classdefs.Rocket \
+                    (None, None, robot.rect.center, pygame.mouse.get_pos(), 10, 5)
+                rocket_img.add(rocket)
+                rocket_hitbox.add(tracer1)
+                allSprites = pygame.sprite.OrderedUpdates \
+                    (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies, wave_text, powerups,
+                    explosions)
+            elif gotplasmarifle == True and weaponselected == 5:
+                rapidfire2= True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if weaponselected == 3:
+               rapidfire= False
+            if weaponselected==5:
+                rapidfire2=False
+
+    if rapidfire:
+        machinegundelay += 1
+    # delay by %3
+        if machinegundelay % 5 == 0:
+            fire.play()
+            bullet1 = classdefs.Bullet(pygame.image.load('./main/shot.png'), robot.getangle(), \
+            robot.rect.center, pygame.mouse.get_pos(), 10, 5)
+            bullet2 = classdefs.Bullet(None, None, robot.rect.center, pygame.mouse.get_pos(), 10,7)
             bullet_img.add(bullet1)
             bullet_hitbox.add(bullet2)
             allSprites = pygame.sprite.OrderedUpdates \
-                (bullet_img, bullet_hitbox,robot,enemies)
-    if x<2:
-        enemy1 = classdefs.enemy(pygame.image.load('./main/temp_sprite.png'),robot.rect.center,screen)
-        enemies.add(enemy1)
-        allSprites = pygame.sprite.OrderedUpdates \
-            (bullet_img, bullet_hitbox, robot, enemies)
-        x+=1
+                (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies, wave_text, powerups,
+                explosions)
+    if rapidfire2:
+        plasmarifledelay += 1
+    # delay by %3
+        if plasmarifledelay % 5 == 0:
+            fireplasma.play()
+            bullet1 = classdefs.Bullet(pygame.image.load('./main/plasmabolt.png'), robot.getangle(), \
+            robot.rect.center, pygame.mouse.get_pos(), 10, 10)
+            bullet2 = classdefs.Bullet(None, None, robot.rect.center, pygame.mouse.get_pos(), 10,10)
+            bullet_img.add(bullet1)
+            bullet_hitbox.add(bullet2)
+            allSprites = pygame.sprite.OrderedUpdates \
+                (bullet_img, bullet_hitbox, robot_group, rocket_hitbox, rocket_img, enemies, wave_text, powerups,
+                explosions)
     for enemy in enemies:
         enemy.rotate(robot.rect.center)
         enemy.set_step_amount(robot.rect.center)
+    c = pygame.sprite.groupcollide(bullet_hitbox, enemies,True, False)
+    v = pygame.sprite.groupcollide(bullet_img, enemies, True, False)
+    if c:
+        for bullet in c.keys():
+            if not (c[bullet][0].damage(bullet.getdamage())):
+                c[bullet][0].kill()
+                enemy1 = classdefs.enemy(pygame.image.load('./main/temp_sprite.png'), robot.rect.center, screen)
+                enemies.add(enemy1)
+                score+=100
+    r = pygame.sprite.groupcollide(rocket_hitbox, enemies, True, False)
+    s = pygame.sprite.groupcollide(rocket_img, enemies, True, False)
+    if r:
+        for rockets in r.keys():
+            if not (r[rockets][0].damage(20)):
+                r[rockets][0].kill()
+                explode.play(0)
+                enemy1 = classdefs.enemy(pygame.image.load('./main/temp_sprite.png'), robot.rect.center, screen)
+                enemies.add(enemy1)
+                score += 100
+            else:
+                explode.play(0)
+
+    e = pygame.sprite.groupcollide(enemies,robot_group,False,False)
+    if e:
+        for rockets in e.keys():
+            if not (robot.takedamage(20)):
+                robot.kill()
+                playerdeath.play(0)
+                running==False
+                screen.fill([255,255,255])
+                GameOver(score,screen)
+            else:
+                score -= 10
+                pain.play(0)
+    p = pygame.sprite.groupcollide(robot_group, powerups, False, False)
+    if p:
+        shotgun.kill()
+        gotshotgun=True
+    wave_text.set_variable(str(score))
     screen.fill([255,255,255])
     allSprites.update()
     allSprites.draw(screen)
